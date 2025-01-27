733c7b605496d6c9bea8915285da82a9082daf38
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-01-27 21:01:30 +0200

    updates

---
+++ src/gr/aueb/cf/mobilecontacts/exceptions/PhoneNumberAlreadyExists.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.mobilecontacts.exceptions;
+
+public class PhoneNumberAlreadyExists extends Exception {
+
+    public PhoneNumberAlreadyExists(String message){
+        super(message);
+    }
+
+}
---
+++ src/gr/aueb/cf/mobilecontacts/service/IMobileContactService.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.mobilecontacts.service;
+
+import gr.aueb.cf.mobilecontacts.dto.MobileContactInsertDTO;
+import gr.aueb.cf.mobilecontacts.dto.MobileContactUpdateDTO;
+import gr.aueb.cf.mobilecontacts.exceptions.ContactNotFoundException;
+import gr.aueb.cf.mobilecontacts.exceptions.PhoneNumberAlreadyExists;
+import gr.aueb.cf.mobilecontacts.model.MobileContact;
+
+import java.util.List;
+
+public interface IMobileContactService {
+
+   MobileContact insertMobileContact(MobileContactInsertDTO dto) throws PhoneNumberAlreadyExists;
+
+   MobileContact updateMobileContact(MobileContactUpdateDTO dto) throws PhoneNumberAlreadyExists, ContactNotFoundException;
+
+   void deleteContactById(Long id) throws ContactNotFoundException;
+
+   MobileContact getContactById(Long id) throws ContactNotFoundException;
+
+   List<MobileContact> getAllContacts();
+}
